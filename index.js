51436f922697f51baef9be133dea163c4d8dd6bf
Jarrett Bishop <jarrettwbishop@gmail.com>
2024-09-08 13:29:01 -0400

    Init

---
+++ index.js
@@ -0,0 +1,45 @@
+class HashMap{
+    constructor() {
+
+    }
+
+    hash(key) {
+
+    }
+
+    set(key, value) {
+
+    }
+
+    get(key) {
+
+    }
+
+    has(key) {
+
+    }
+
+    remove(key) {
+
+    }
+
+    length() {
+
+    }
+
+    clear() {
+
+    }
+
+    keys() {
+
+    }
+
+    values() {
+
+    }
+
+    entries() {
+        
+    }
+}
